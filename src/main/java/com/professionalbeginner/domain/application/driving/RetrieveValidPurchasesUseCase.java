db022c4f118433193c11392c19c1f798024ef4d4
Florian Kempenich <shockn745@gmail.com>
2016-06-09 21:17:59 +0200

    Add use case skeletton

--- src/main/java/com/professionalbeginner/domain/application/driving/RetrieveValidPurchasesUseCase.java
+++ src/main/java/com/professionalbeginner/domain/application/driving/RetrieveValidPurchasesUseCase.java
@@ -1,9 +1,13 @@
 package com.professionalbeginner.domain.application.driving;
 
+import com.professionalbeginner.domain.application.DetailsMapper;
+import com.professionalbeginner.domain.application.PurchaseMapper;
 import com.professionalbeginner.domain.application.driven.PurchaseDetailsRepository;
 import com.professionalbeginner.domain.application.driven.PurchaseRepository;
 import com.professionalbeginner.domain.application.driven.PurchaseSerializer;
 import com.professionalbeginner.domain.core.validator.PurchaseValidator;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  *
@@ -11,17 +15,24 @@ import com.professionalbeginner.domain.core.validator.PurchaseValidator;
 public class RetrieveValidPurchasesUseCase implements UseCase<String> {
 
     private final PurchaseRepository purchaseRepository;
+    private final PurchaseMapper purchaseMapper;
     private final PurchaseDetailsRepository detailsRepository;
+    private final DetailsMapper detailsMapper;
     private final PurchaseValidator validator;
     private final PurchaseSerializer serializer;
 
     public RetrieveValidPurchasesUseCase(PurchaseRepository purchaseRepository,
+                                         PurchaseMapper purchaseMapper,
                                          PurchaseDetailsRepository detailsRepository,
-                                         PurchaseValidator validator, PurchaseSerializer serializer) {
-        this.purchaseRepository = purchaseRepository;
-        this.detailsRepository = detailsRepository;
-        this.validator = validator;
-        this.serializer = serializer;
+                                         DetailsMapper detailsMapper,
+                                         PurchaseValidator validator,
+                                         PurchaseSerializer serializer) {
+        this.purchaseRepository = checkNotNull(purchaseRepository);
+        this.purchaseMapper = checkNotNull(purchaseMapper);
+        this.detailsRepository = checkNotNull(detailsRepository);
+        this.detailsMapper = checkNotNull(detailsMapper);
+        this.validator = checkNotNull(validator);
+        this.serializer = checkNotNull(serializer);
     }
 
     @Override
---
+++ src/test/java/com/professionalbeginner/domain/application/driving/RetrieveValidPurchasesUseCaseTest.java
@@ -0,0 +1,65 @@
+package com.professionalbeginner.domain.application.driving;
+
+import com.professionalbeginner.data.detail.FakePurchaseDetailsRepository;
+import com.professionalbeginner.data.purchase.FakePurchaseRepository;
+import com.professionalbeginner.domain.application.DetailsMapper;
+import com.professionalbeginner.domain.application.PurchaseMapper;
+import com.professionalbeginner.domain.application.driven.PurchaseDetailsRepository;
+import com.professionalbeginner.domain.application.driven.PurchaseRepository;
+import com.professionalbeginner.domain.application.driven.PurchaseSerializer;
+import com.professionalbeginner.domain.core.DetailsFactory;
+import com.professionalbeginner.domain.core.PurchaseFactory;
+import com.professionalbeginner.domain.core.validator.PurchaseValidator;
+import com.professionalbeginner.domain.core.validator.ValidateAllValidator;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Florian on 09/06/16.
+ */
+public class RetrieveValidPurchasesUseCaseTest {
+
+    RetrieveValidPurchasesUseCase useCase;
+
+    PurchaseRepository purchaseRepository;
+    PurchaseDetailsRepository detailsRepository;
+    PurchaseMapper purchaseMapper;
+    DetailsMapper detailsMapper;
+    PurchaseValidator validator;
+
+    @Mock
+    PurchaseSerializer serializer;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        validator = new ValidateAllValidator();
+
+        DetailsFactory detailsFactory = new DetailsFactory();
+        PurchaseFactory purchaseFactory = new PurchaseFactory(validator);
+
+        purchaseRepository = new FakePurchaseRepository();
+        detailsMapper = new DetailsMapper(detailsFactory);
+        purchaseMapper = new PurchaseMapper(purchaseFactory, detailsMapper);
+        detailsRepository = new FakePurchaseDetailsRepository();
+
+        useCase = new RetrieveValidPurchasesUseCase(
+                purchaseRepository,
+                purchaseMapper,
+                detailsRepository,
+                detailsMapper,
+                validator,
+                serializer
+        );
+    }
+
+    @Test
+    public void name() throws Exception {
+
+    }
+}
